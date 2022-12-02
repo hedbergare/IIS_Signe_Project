8f863527c70f55aa08058917f3f79f17b98c11ce
dmkslice <39928755+dmkslice@users.noreply.github.com>
2022-12-02 05:02:37 +0100

    arbete hittils på Unet

---
+++ LE/Unet.py
@@ -0,0 +1,70 @@
+
+import torch
+from torch import nn
+from torchvision import  transforms
+
+
+# mellan varje nivå i u net har man ett
+class Block(nn.Module):
+    def __init__(self, in_ch: int, out_ch: int):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_ch, out_ch, 3)
+        self.relu = nn.ReLU()
+        self.conv2 = nn.Conv2d(out_ch, out_ch, 3)
+    def forward(self, x):
+        return self.conv2(self.relu(self.conv1(x)))
+
+
+class UNetModel(nn.Module):
+    def __init__(
+        self, encodingLayersSizes: list[list[int]], 
+        decodingLayesSizes: list[list[int]], ):
+        super().__init__()
+    
+        self.encodingLayersSizes = encodingLayersSizes
+        self.decodingLayesSizes = decodingLayesSizes
+        self.encBlocks = nn.ModuleList()
+        self.decBlocks = nn.ModuleList()
+        self.upconvs = nn.ModuleList()
+        self.maxPool=nn.MaxPool2d(2)
+        self.encodingDropoutList=nn.ModuleList()
+        self.decodingDropoutList=nn.ModuleList()
+
+
+        for layer in self.encodingLayersSizes:
+            self.encBlocks.append(Block(layer[0], layer[1]))
+
+        for layer in self.decodingLayesSizes:
+            self.decBlocks.append(Block(layer[0], layer[1]))
+            self.upconvs.append(nn.ConvTranspose2d(layer[0], layer[1], 2, 2))
+
+        
+
+
+        #kan inte komma på hur man ska göra sista ouputlayer 
+        self.finalayers=None
+
+    def cropthing(self, encodingFeature,x):
+        encodingFeature=transforms.CenterCrop([x.shape()[2] ,x.shape()[3]])(encodingFeature)
+        return encodingFeature
+
+    def forward(self, x):
+        encodingFeatures=[]
+        for layer in self.encBlocks:
+            x = layer(x)
+            encodingFeatures.append(x)
+            x = self.maxPool(x)
+        
+        for layer, in zip(self.decBlocks,self.upconvs):
+            x=layer[1](x)
+            encodingFeature=encodingFeatures.pop()
+            encodingFeature=self.cropthing(self, encodingFeature,x)
+            x = torch.cat([x, encodingFeature], dim=1)
+            x=layer[0](x)
+        
+
+
+
+
+
+        return self.finalayers(x)
