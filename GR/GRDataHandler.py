dd7aa86951e8d4399ddf1feb55bbb24bf7b30d33
hedbergare <hedbergjacob@gmail.com>
2023-01-08 15:18:05 +0100

    Finalized SVM model and test file prepared

--- GR/GRDataHandler.py
+++ GR/GRDataHandler.py
@@ -2,24 +2,22 @@ import numpy as np
 import os
 
 
-def unison_shuffled_copies(a, b):
-    assert len(a) == len(b)
-    p = np.random.permutation(len(a))
-    return a[p], b[p]
-
-
 def get_GR_data():
     # Read data from files
     path_of_dir = os.getcwd()
     print("DIR PATH", path_of_dir)
-    X_path = os.path.join(path_of_dir, path_of_dir, "GR\\Data\\new_X.npy")
-    Y_path = os.path.join(path_of_dir, path_of_dir, "GR\\Data\\new_Y.npy")
+    X_path = os.path.join(path_of_dir, path_of_dir, "Data\\all_X.npy")
+    Y_path = os.path.join(path_of_dir, path_of_dir, "Data\\all_Y.npy")
     X_data = np.load(X_path, allow_pickle=True)
     Y_data = np.load(Y_path, allow_pickle=True)
 
     # Make them numpy arrays
     X_array = np.array(X_data)
     Y_array = np.array(Y_data)
+
+    print("Mean before", np.mean(X_array))
+    X_array = normalize_by_joint_distance(X_array)
+    print("Mean after", np.mean(X_array))
 
     # Split into training and test sets
     divisor = round(0.8*len(X_array))
@@ -69,16 +67,3 @@ def normalize_by_joint_distance(X_values):
 
 
 X_train, X_test, Y_train, Y_test = get_GR_data()
-print(X_train.shape)
-print(X_test.shape)
-print(Y_train.shape)
-print(Y_test.shape)
-print("Mean:", np.mean(X_train))
-print("Max:", np.max(X_train))
-print("Min:", np.min(X_train))
-#X_train = normalize_data(X_train, Y_train)
-X_train = normalize_by_joint_distance(X_train)
-print("Mean after:", np.mean(X_train))
-print("Max after:", np.max(X_train))
-print("Min after:", np.min(X_train))
-# print(np.mean(X_train))
